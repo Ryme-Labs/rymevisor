2bd1fbe90f0cc4f9e367d073bc55cd1ed6c3cfdc
Ryme Labs <dev@rymelabs.dev>
2026-08-01 17:00:00 +0100

    feat: add logging

---
+++ internal/logging/logging.go
@@ -0,0 +1,57 @@
+package logging
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func New(level, format string) (*zap.Logger, error) {
+	var cfg zap.Config
+	if format == "console" {
+		cfg = zap.NewDevelopmentConfig()
+	} else {
+		cfg = zap.NewProductionConfig()
+	}
+
+	lvl := zapcore.InfoLevel
+	if err := lvl.UnmarshalText([]byte(level)); err == nil {
+		cfg.Level = zap.NewAtomicLevelAt(lvl)
+	}
+
+	if format == "console" {
+		cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+		cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	}
+
+	logger, err := cfg.Build(zap.AddCallerSkip(0))
+	if err != nil {
+		return nil, err
+	}
+
+	return logger, nil
+}
+
+func NewNop() *zap.Logger {
+	return zap.NewNop()
+}
+
+func Must(level, format string) *zap.Logger {
+	l, err := New(level, format)
+	if err != nil {
+		panic(err)
+	}
+	return l
+}
+
+func WithStdout() *zap.Logger {
+	return zap.New(
+		zapcore.NewCore(
+			zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+			zapcore.Lock(os.Stdout),
+			zapcore.InfoLevel,
+		),
+		zap.AddCaller(),
+	)
+}
